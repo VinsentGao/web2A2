32141b87f574fe699120248c88939a7dadf9bbbd
VinsentGao <j.gao.25@student.scu.edu.au>
2025-10-04 15:05:53 +0800

    Update event_db.js
    fix bug

--- charity-events-project/api/event_db.js
+++ charity-events-project/api/event_db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
-    password: '',  // XAMPP 默认空密码
+    password: '',  // XAMPP 
     database: 'charityevents_db',
     waitForConnections: true,
     connectionLimit: 10,
